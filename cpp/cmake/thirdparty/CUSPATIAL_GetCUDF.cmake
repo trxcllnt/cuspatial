ca414599c0c31ba9ea92c7fab122a901d6ea3245
ptaylor <paul.e.taylor@me.com>
2021-02-11 16:04:01 -0600

    use a special branch on my cudf fork

--- cpp/cmake/thirdparty/CUSPATIAL_GetCUDF.cmake
+++ cpp/cmake/thirdparty/CUSPATIAL_GetCUDF.cmake
@@ -18,11 +18,11 @@ function(find_and_configure_cudf VERSION)
     CPMFindPackage(NAME cudf
         VERSION         ${VERSION}
         # GIT_REPOSITORY https://github.com/rapidsai/cudf.git
-        # GIT_REPOSITORY  https://github.com/trxcllnt/cudf.git
+        GIT_REPOSITORY  https://github.com/trxcllnt/cudf.git
         # Can also use a local path to your repo clone for testing
-        GIT_REPOSITORY  /home/ptaylor/dev/rapids/cudf
+        # GIT_REPOSITORY  /home/ptaylor/dev/rapids/cudf
         # GIT_TAG        branch-${VERSION}
-        GIT_TAG         fix/cmake-exports
+        GIT_TAG         combined-fixes
         GIT_SHALLOW     TRUE
         SOURCE_SUBDIR   cpp
         OPTIONS         "BUILD_TESTS OFF"
